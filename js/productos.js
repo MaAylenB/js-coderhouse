f329d49b284cf09165d07010b4284cee2fc057a3
María Aylén Benavidez <maaylenbenavidez@gmail.com>
2023-12-15 22:42:25 -0300

    finprueba

---
+++ js/productos.js
@@ -0,0 +1,182 @@
+[
+    {
+        "id": "arrastre-01",
+        "titulo": "arrastre 01",
+        "imagen": "./img/arrastres/01.jpg",
+        "categoria": {
+            "nombre": "arrastres",
+            "id": "arrastres"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "arrastre-02",
+        "titulo": "arrastre 02",
+        "imagen": "./img/arrastres/02.jpg",
+        "categoria": {
+            "nombre": "arrastres",
+            "id": "arrastres"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "arrastre-03",
+        "titulo": "arrastre 03",
+        "imagen": "./img/arrastres/03.jpg",
+        "categoria": {
+            "nombre": "arrastres",
+            "id": "arrastres"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "arrastre-04",
+        "titulo": "arrastre 04",
+        "imagen": "./img/arrastres/04.jpg",
+        "categoria": {
+            "nombre": "arrastres",
+            "id": "arrastres"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "arrastre-05",
+        "titulo": "arrastre 05",
+        "imagen": "./img/arrastres/05.jpg",
+        "categoria": {
+            "nombre": "arrastres",
+            "id": "arrastres"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "disfraces-01",
+        "titulo": "disfraces 01",
+        "imagen": "./img/disfraces/01.jpg",
+        "categoria": {
+            "nombre": "disfraces",
+            "id": "disfraces"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "disfraces-02",
+        "titulo": "disfraces 02",
+        "imagen": "./img/disfraces/02.jpg",
+        "categoria": {
+            "nombre": "disfraces",
+            "id": "disfraces"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "disfraces-03",
+        "titulo": "disfraces 03",
+        "imagen": "./img/disfraces/03.jpg",
+        "categoria": {
+            "nombre": "disfraces",
+            "id": "disfraces"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "disfraces-04",
+        "titulo": "disfraces 04",
+        "imagen": "./img/disfraces/04.jpg",
+        "categoria": {
+            "nombre": "disfraces",
+            "id": "disfraces"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "disfraces-05",
+        "titulo": "disfraces 05",
+        "imagen": "./img/disfraces/05.jpg",
+        "categoria": {
+            "nombre": "disfraces",
+            "id": "disfraces"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "disfraces-06",
+        "titulo": "disfraces 06",
+        "imagen": "./img/disfraces/06.jpg",
+        "categoria": {
+            "nombre": "disfraces",
+            "id": "disfraces"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "disfraces-07",
+        "titulo": "disfraces 07",
+        "imagen": "./img/disfraces/07.jpg",
+        "categoria": {
+            "nombre": "disfraces",
+            "id": "disfraces"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "disfraces-08",
+        "titulo": "disfraces 08",
+        "imagen": "./img/disfraces/08.jpg",
+        "categoria": {
+            "nombre": "disfraces",
+            "id": "disfraces"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "herramientas-01",
+        "titulo": "herramientas 01",
+        "imagen": "./img/herramientas/01.jpg",
+        "categoria": {
+            "nombre": "herramientas",
+            "id": "herramientas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "herramientas-02",
+        "titulo": "herramientas 02",
+        "imagen": "./img/herramientas/02.jpg",
+        "categoria": {
+            "nombre": "herramientas",
+            "id": "herramientas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "herramientas-03",
+        "titulo": "herramientas 03",
+        "imagen": "./img/herramientas/03.jpg",
+        "categoria": {
+            "nombre": "herramientas",
+            "id": "herramientas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "herramientas-04",
+        "titulo": "herramientas 04",
+        "imagen": "./img/herramientas/04.jpg",
+        "categoria": {
+            "nombre": "herramientas",
+            "id": "herramientas"
+        },
+        "precio": 1000
+    },
+    {
+        "id": "herramientas-05",
+        "titulo": "herramientas 05",
+        "imagen": "./img/herramientas/05.jpg",
+        "categoria": {
+            "nombre": "herramientas",
+            "id": "herramientas"
+        },
+        "precio": 1000
+    }
+]
